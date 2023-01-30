a2532d90f6bfd7f6a87447baf0ae58f67974bccd
Kieran Wales <17WalesK@fulford.york.sch.uk>
2023-01-30 11:18:25 +0000

    first thing ive ever committed to

---
+++ Server/Server/static/scripts.js
@@ -0,0 +1,38 @@
+const messageBox = document.getElementById("message");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+function reset() {
+
+}
+
+function createUser() {
+
+}
+
+function validate() {
+    presenceCheck(username, "Please enter a username", messageBox);
+    presenceCheck(password, "Please enter a password", messageBox);
+    lengthCheck(username, 3, 8, "Username should be between 3 and 8 characters", messageBox)
+    lengthCheck(password, 3, 8, "Password should be between 3 and 8 characters", messageBox)
+}
+
+function lengthCheck(input, min, max, message, output) {
+    if (input.value.length > max || input.value.length < min) {
+        output.innerText = message;
+        throw message;
+    }
+    else {
+        output.innerText = "Login to Blendr";
+    }
+}
+
+function presenceCheck(input, message, output) {
+    if (input.value == "") {
+        output.innerText = message;
+        throw message;
+    }
+    else {
+        output.innerText = "Login to Blendr";
+    }
+}
